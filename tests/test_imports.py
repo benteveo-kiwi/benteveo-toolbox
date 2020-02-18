e5770c9eff40514d57968a8c83a49148cec91815
Pedro Worcel <pedro@benteveo.kiwi>
2020-02-19 08:03:58 +1300

    Remove test for now as I've added an issue for it in the GH tracker. Update jar files with new features and bugfixes.

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -11,11 +11,3 @@ class TestImports(BaseTestClass):
         self.assertEquals(len(burpExtension.getScannerChecks()), 1)
         self.assertEquals(len(burpExtension.getExtensionStateListeners()), 2)
         self.assertEquals(len(burpExtension.getContextMenuFactories()), 1)
-
-    def testImportResourcesDontCollide(self):
-        """
-        Sometimes imported JARs have resources, such as wordlists with the same names. This results in runtime errors that are hard to detect. The same can be said for class names.
-
-        This test checks that this doesn't happen.
-        """
-        self.assertTrue(False)
